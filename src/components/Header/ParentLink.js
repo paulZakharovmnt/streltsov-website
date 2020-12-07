0fac1c35e1bfe0c2ca6705266221645af0fdd328
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-07 14:58:27 -0500

    Crate parent basic links components

---
+++ src/components/Header/ParentLink.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ChildLinks from "./ChildLinks";
+import classNames from "classnames";
+
+const ParentLink = ({
+  handleShowChildLinksClick,
+  parentLink,
+  showingParentLinkList,
+  childLinks,
+}) => {
+  const lineClasses = classNames("border-under-link", {
+    openedLine: showingParentLinkList === parentLink,
+  });
+  const angleDownClasses = classNames("arrow-btn", "fas fa-angle-down", {
+    angleClose: showingParentLinkList === parentLink,
+  });
+
+  const parentLinkClasses = classNames("parent-link", {
+    openedParent: showingParentLinkList === parentLink,
+  });
+  return (
+    <div className="parent-link-container">
+      <div
+        className={parentLinkClasses}
+        onClick={() => handleShowChildLinksClick(parentLink)}
+      >
+        <p>{parentLink}</p>
+        <i className={angleDownClasses} />
+      </div>
+      <span className={lineClasses}></span>
+
+      {showingParentLinkList === parentLink && (
+        <ChildLinks
+          childLinks={childLinks}
+          showingParentLinkList={showingParentLinkList}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ParentLink;
