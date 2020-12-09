e21f6a288860a098c7e151f8a6b6e6433043b99c
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-09 11:32:23 -0500

    Create Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer">
+        <h5>(С) 2018-2020, STRELTSOV.ART</h5>
+        <div className="logo"></div>
+        <div className="footer-links-container">Links</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
