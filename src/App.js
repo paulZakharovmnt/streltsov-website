2b0d1ddda4da8eb6e5e2936febcd1b3a17c5d516
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-09 11:31:35 -0500

    Import Router and create Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Header/Nav";
+import Swiper from "./components/Swiper";
+import About from "./Pages/About";
+import Cart from "./Pages/Cart";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Nav />
-      </header>
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <Nav />
+        </header>
+        <Switch>
+          <Route path="/" component={Swiper} exact />
+          <Route path="/about" component={About} />
+          <Route path="/cart" component={Cart} />
+        </Switch>
+        {/* <main>
+          <Swiper />
+        </main> */}
+        <footer>
+          <Footer />{" "}
+        </footer>
+      </div>
+    </Router>
   );
 }
 
