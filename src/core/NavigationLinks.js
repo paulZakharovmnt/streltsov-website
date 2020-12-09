0c1bfdff3f71467327c680292261895f27deb3b7
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-09 11:32:57 -0500

    Put list of menu links in separate file

---
+++ src/core/NavigationLinks.js
@@ -0,0 +1,15 @@
+export const linksWithOptions = ["Pen", "Jewelry", "Accessory"];
+export const directLinks = ["Gallery", "About"];
+export const childLinks = {
+  Pen: ["BallPoint Pens", "Fountain Pens", "Pencils", "Limited Edition"],
+  Jewelry: ["Pendants"],
+  Accessory: [
+    "Bracelets",
+    "Lighters",
+    "Cufflinks",
+    "Keychains",
+    "Beads",
+    "Dices",
+    "MadRussianheads.com",
+  ],
+};
