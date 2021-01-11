3a68aa9cab0d09303981ac198051aeb527c709b6
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-01-11 11:04:38 -0500

    Create actions file with basic actions

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,39 @@
+export const setProductsToStateFromFB = (products) => {
+  return {
+    type: "SET_ALL_PRODUCTS_FROM_FB",
+    payload: products,
+  };
+};
+
+export const addProductToCart = (product) => {
+  return {
+    type: "ADD_PRODUCT_TO_CART",
+    payload: product,
+  };
+};
+
+export const deleteProductFromCart = (product) => {
+  return {
+    type: "DELETE_PRODUCT_FROM_CART",
+    payload: product,
+  };
+};
+export const clearCart = () => {
+  return {
+    type: "CLEAR_ALL_CART",
+  };
+};
+
+export const incrementProductQuontity = (product) => {
+  return {
+    type: "INCREMENT_PRODUCT_QUONTITY",
+    payload: product,
+  };
+};
+
+export const decrementProductQuontity = (product) => {
+  return {
+    type: "DECREMENT_PRODUCT_QUONTITY",
+    payload: product,
+  };
+};
