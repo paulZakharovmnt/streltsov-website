6c5868949a3aba9a94aa06699562c8cbcc070249
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-01-11 11:04:54 -0500

    Create test-file with products

---
+++ src/core/TestProducts.js
@@ -0,0 +1,64 @@
+import image from "../img/Products/DSC03497.jpg";
+export const productsAllIds = ["Okusaka", "Onimusha", "Hitori", "Jet Lee"];
+export const productsByIdTEST = {
+  Okusaka: {
+    name: "Okusaka",
+    collection: "Samurai",
+    type: "Pen",
+    id: 12221,
+    price: 400,
+    description: "Amazing pen made using",
+    ttx: {
+      width: 122,
+      weight: 200,
+    },
+    image: "https://streltsov.art/wp-content/uploads/2018/11/2.jpg",
+    images: ["link1", "link2", "link3"],
+    inStock: true,
+  },
+  Onimusha: {
+    name: "Onimusha",
+    collection: "Ninja",
+    type: "Pencil",
+    id: 12222,
+    price: 500,
+    description: "From new collection",
+    ttx: {
+      width: 122,
+      weight: 200,
+    },
+    image: "https://streltsov.art/wp-content/uploads/2018/11/26-10.jpg",
+    images: ["link1", "link2", "link3"],
+    inStock: true,
+  },
+  Hitori: {
+    name: "Hitori",
+    collection: "Ninja",
+    type: "Pen",
+    id: 12223,
+    price: 600,
+    description: "Cool pen",
+    ttx: {
+      width: 122,
+      weight: 200,
+    },
+    image: "https://streltsov.art/wp-content/uploads/2018/11/DSC02013.jpg",
+    images: ["link1", "link2", "link3"],
+    inStock: true,
+  },
+  "Jet Lee": {
+    name: "Jet Lee",
+    collection: "Samurai",
+    type: "Pencil",
+    id: 12224,
+    price: 700,
+    description: "Cool pen",
+    ttx: {
+      width: 122,
+      weight: 200,
+    },
+    image: "https://streltsov.art/wp-content/uploads/2018/11/DSC03275.jpg",
+    images: ["link1", "link2", "link3"],
+    inStock: true,
+  },
+};
