3077606eac501a0f8bcf923321b9006095ee86a9
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-01-11 11:07:57 -0500

    Create navigation arrow component

---
+++ src/components/Button/NavArrow.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NavArrow.css";
+
+const NavArrow = () => {
+  return (
+    <a href="#" className="arrow-container">
+      <div className="arrow"></div>
+      <div className="arrow"></div>
+      <div className="arrow"></div>
+    </a>
+  );
+};
+
+export default NavArrow;
