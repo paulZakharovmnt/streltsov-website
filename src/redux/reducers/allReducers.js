99ceb69b22d9f15553a350dbafbbde3f2f233d62
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-01-18 09:20:43 -0500

    Add filters reducer to list of reducers

--- src/redux/reducers/allReducers.js
+++ src/redux/reducers/allReducers.js
@@ -3,12 +3,14 @@ import productsAllIdsReducer from "./productsAllIdsReducer";
 import productsByIdReducer from "./productsByIdReducer";
 import productsAllIdsInCartReducer from "./cartReducers/productsAllIdsInCartReducer";
 import cartReducer from "./cartReducers/cartReducer";
+import filtersReducer from "./Filters/filtersReducer";
 
 const allReducers = combineReducers({
   productsAllIdsReducer,
   productsByIdReducer,
   cartReducer,
   productsAllIdsInCartReducer,
+  filtersReducer,
 });
 
 export default allReducers;
