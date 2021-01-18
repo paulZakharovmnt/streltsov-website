c77e3dd4af6526666bafc3183b2bce6a3d934d60
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-01-18 09:23:20 -0500

    Add new container

--- src/components/Cart/ProductInCart.js
+++ src/components/Cart/ProductInCart.js
@@ -16,39 +16,41 @@ const ProductInCart = ({ product, productsInCart }) => {
       <div className="cart-product-image">
         <img />
       </div>
-      <div className="product-name-price">
-        <h2>{product}</h2>
-        <p>{productInfo.description}</p>
-        <p>$ {productInfo.price}</p>
-      </div>
-      <div className="product-quont-remove">
-        <div className="cart-quontity-container">
-          <div className="minus-cont">
-            {productInfo.quontity > 1 && (
-              <i
-                className="quont-control fas fa-minus"
-                onClick={() => dispatch(decrementProductQuontity(product))}
-              />
-            )}
-          </div>
-          <div className="quont-cont">{productInfo.quontity}</div>
+      <div className="cart-product-data">
+        <div className="product-name-price">
+          <h2>{product}</h2>
+          <p>{productInfo.description}</p>
+          <p>$ {productInfo.price}</p>
+        </div>
+        <div className="product-quont-remove">
+          <div className="cart-quontity-container">
+            <div className="minus-cont">
+              {productInfo.quontity > 1 && (
+                <i
+                  className="quont-control fas fa-minus"
+                  onClick={() => dispatch(decrementProductQuontity(product))}
+                />
+              )}
+            </div>
+            <div className="quont-cont">{productInfo.quontity}</div>
 
-          <div className="plus-cont">
-            <i
-              className="quont-control fas fa-plus"
-              onClick={() => dispatch(incrementProductQuontity(product))}
-            />
+            <div className="plus-cont">
+              <i
+                className="quont-control fas fa-plus"
+                onClick={() => dispatch(incrementProductQuontity(product))}
+              />
+            </div>
+          </div>
+          <div
+            className="remove-btn"
+            onClick={() => dispatch(deleteProductFromCart(product))}
+          >
+            Remove
           </div>
         </div>
-        <div
-          className="remove-btn"
-          onClick={() => dispatch(deleteProductFromCart(product))}
-        >
-          Remove
+        <div className="product-final-price">
+          <h4>$ {productInfo.price * productInfo.quontity}</h4>
         </div>
-      </div>
-      <div className="product-final-price">
-        <h4>$ {productInfo.price * productInfo.quontity}</h4>
       </div>
     </div>
   );
