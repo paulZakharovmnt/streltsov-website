80e833c522384e258aee1d56d1c134c269798871
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-01-18 09:23:29 -0500

    Add new containers

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,22 +11,25 @@ const Cart = () => {
   );
 
   return (
-    <div className="cart-container">
-      <div className="cart-list-of-products">
-        {productsAllIdsInCart.length === 0 ? (
-          <div>YOUR CART IS EMPTY</div>
-        ) : (
-          productsAllIdsInCart.map((product) => (
-            <ProductInCart
-              key={product}
-              product={product}
-              productsInCart={productsInCart}
-            />
-          ))
-        )}
-      </div>
-      <div className="cart-subtotal-container">
-        <h3>Subtotal</h3>
+    <div className="cart-page">
+      <div className="cart-header"> Your cart: </div>
+      <div className="cart-container">
+        <div className="cart-list-of-products">
+          {productsAllIdsInCart.length === 0 ? (
+            <div>YOUR CART IS EMPTY</div>
+          ) : (
+            productsAllIdsInCart.map((product) => (
+              <ProductInCart
+                key={product}
+                product={product}
+                productsInCart={productsInCart}
+              />
+            ))
+          )}
+        </div>
+        <div className="cart-subtotal-container">
+          <h3>Subtotal</h3>
+        </div>
       </div>
     </div>
   );
