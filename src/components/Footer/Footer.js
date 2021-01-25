4d311b01883747f1f6d935524df0b56adef03660
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-01-25 10:18:54 -0500

    Add Icons to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SocialMediaIcons from "../AdditionalComponents/SocialMediaIcons";
 import "./Footer.css";
 
 const Footer = () => {
@@ -7,7 +8,9 @@ const Footer = () => {
       <div className="footer">
         <h5>(С) 2018-2020, STRELTSOV.ART</h5>
         <div className="logo"></div>
-        <div className="footer-links-container">Links</div>
+        <div className="footer-links-container">
+          <SocialMediaIcons />{" "}
+        </div>
       </div>
     </div>
   );
