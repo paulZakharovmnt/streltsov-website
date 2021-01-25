99e90449916eb3e8c39c7f4b829acc96c8ec0795
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-01-25 10:18:29 -0500

    Made button more reusable

--- src/components/AdditionalComponents/GoBackBTN.js
+++ src/components/AdditionalComponents/GoBackBTN.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./GoBackBTN.css";
 import { Link } from "react-router-dom";
 
-const GoBackBTN = () => {
+const GoBackBTN = ({ whereToLink, linkName }) => {
   return (
     <div className="go-back-btn">
-      <Link className="backLink" to={`/products`}>
-        Back to List
+      <Link className="backLink" to={whereToLink}>
+        {linkName}
       </Link>
     </div>
   );
