e0dbdaa3ffd4790aba696e8d8e1617e7bda3281e
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-01-25 10:20:34 -0500

    Fixed small bugs

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,24 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./Products.css";
 import FilterBar from "./Filters/FilterBar";
 
 const Products = () => {
+  const { childLink } = useParams();
   const filtersState = useSelector((state) => state.filtersReducer);
   const productsById = useSelector((state) => state.productsByIdReducer);
   const productsAllIds = useSelector((state) => state.productsAllIdsReducer);
 
+  console.log(childLink);
+
+  // console.log(
+  //   productsAllIds.sort((a, b) => {
+  //     return productsById[a].price > productsById[b].price
+  //       ? -1
+  //       : productsById[b].price > productsById[a].price
+  //       ? 1
+  //       : 0;
+  //   })
+  // );
+
   return (
     <div className="products-page">
       <div className="products-header">
-        <h1>PENS</h1>
+        <h1>{childLink}</h1>
       </div>
       <FilterBar />
 
       <div className="list-of-products-container">
         <div className="list-of-products">
           {productsAllIds
+            .filter((product) => productsById[product].type === childLink)
             .filter((product) =>
               filtersState.byMaterial
                 ? productsById[product].material === filtersState.byMaterial
@@ -29,6 +44,7 @@ const Products = () => {
                 ? productsById[product].collection === filtersState.byCollection
                 : true
             )
+
             .map((product) => (
               <div key={product} className="product-container">
                 <img src={productsById[product].image} alt="" />
@@ -36,11 +52,14 @@ const Products = () => {
                   <h3>{productsById[product].name}</h3>
                   <h3>Price: {productsById[product].price}</h3>
                   <h3>
-                    <Link to={`/products/${product}`}>See details</Link>
+                    <Link to={`/products/${childLink}/${product}`}>
+                      See details
+                    </Link>
                   </h3>
                 </div>
               </div>
             ))}
+          {/* <div className="white-cube-test"></div> */}
         </div>
       </div>
     </div>
