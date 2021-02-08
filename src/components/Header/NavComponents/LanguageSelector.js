cf7e0400f89a90b8f7fbb92a7d4b94b8c7a76fa2
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-02-08 11:24:13 -0500

    Crreate lang selector component

---
+++ src/components/Header/NavComponents/LanguageSelector.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LanguageSelector.css";
+
+const LanguageSelector = () => {
+  return <div className="nav-language-selector">EN | FR</div>;
+};
+
+export default LanguageSelector;
