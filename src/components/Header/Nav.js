91e43e63348ccaa5a1697f78ae0b3d1a5229b8cb
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-02-08 11:22:52 -0500

    Split Nav into 2 components

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,55 +1,17 @@
-import React, { useState } from "react";
-import MenuWithLinks from "./MenuWithLinks";
-import classNames from "classnames";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React from "react";
 import "./Nav.css";
+import DesktopNavPanel from "./DesktopNavPanel";
+import MobileNavPanel from "./MobileNavPanel";
 
 const Nav = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const productsAllIdsInCart = useSelector(
-    (state) => state.productsAllIdsInCartReducer
-  );
-  // const itemsInCartLength = useSelector((state) => state.cartReducer.length);
-
-  const burgerBtnClasses = classNames("burger-container", {
-    active: showMenu,
-    close: !showMenu,
-  });
-
-  const itemsInCartCounterClasses = classNames("items-number-in-cart", {
-    cartIsNotEmpty: productsAllIdsInCart.length > 0,
-  });
-
   return (
     <div className="nav">
-      <div className="nav-fix-part">
-        <div
-          className={burgerBtnClasses}
-          onClick={() => setShowMenu(!showMenu)}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <Link to="/">
-          <div className="logo"></div>
-        </Link>
-
-        <div className="cart">
-          <Link to="/cart">
-            <i className="cart-icon fas fa-shopping-cart" />
-            <div className={itemsInCartCounterClasses}>
-              <h4>{productsAllIdsInCart.length}</h4>
-            </div>
-          </Link>
-        </div>
+      <div className="nav-desktop">
+        <DesktopNavPanel />
       </div>
-      {showMenu && (
-        <div className="nav-dynamic-part">
-          <MenuWithLinks setShowMenu={setShowMenu} />
-        </div>
-      )}
+      <div className="nav-mobile">
+        <MobileNavPanel />
+      </div>
     </div>
   );
 };
