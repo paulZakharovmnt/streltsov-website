f089e3f0bbff5706205cbabe89cd6be3d6d2aaf8
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-02-09 09:47:37 -0500

    Add dynamic name in popup

--- src/components/AdditionalComponents/PopupMessage.js
+++ src/components/AdditionalComponents/PopupMessage.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./PopupMessage.css";
 
 const PopupMessage = ({ currentProduct }) => {
+  console.log(currentProduct);
   return (
     <div className="pop-up-message-container">
       <div className="popup-message">
         <p>The product </p>
-        <h5 className="product-name-popup">ONIMUSHA</h5>{" "}
+        <h5 className="product-name-popup">{currentProduct.name}</h5>
         <p> has been added to the cart</p>
       </div>
 
