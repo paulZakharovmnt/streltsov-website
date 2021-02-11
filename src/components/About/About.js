7489ed5fbebd1efcfd8156fe77cd51935430ea29
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-02-11 09:48:54 -0500

    Delete old components and styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import SocialMediaIcons from "../AdditionalComponents/SocialMediaIcons";
+import React from "react";
 import "./About.css";
-import CompanyInfo from "./CompanyInfo";
-import ContactUs from "./ContactUs";
 
 const About = () => {
-  const [contactUsModal, setContactUsModal] = useState(false);
   return (
     <div className="about-page">
       <div className="about-header">About</div>
-      <div className="about-control-btns-container">
-        <div onClick={() => setContactUsModal(false)}>Ifno about company</div>
-        <div onClick={() => setContactUsModal(true)}>Contact us!</div>
-      </div>
-      <div className="about-page-container">
-        {contactUsModal ? <ContactUs /> : <CompanyInfo />}
-      </div>
+      <div className="about-container"></div>
     </div>
   );
 };
