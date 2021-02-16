1d7d6fdcb8bb395692cc058159cc69f580de4e30
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-02-16 10:25:16 -0500

    Change initial state and functions

--- src/redux/reducers/Filters/filtersReducer.js
+++ src/redux/reducers/Filters/filtersReducer.js
@@ -1,17 +1,32 @@
 const initialState = {
-  byPrice: "",
-  byCollection: "",
-  ByPenType: "",
+  collection: {
+    name: "By Collection",
+    value: "",
+  },
+  penType: {
+    name: "By Pen Type",
+    value: "",
+  },
+  price: {
+    name: "By Price",
+    value: "",
+  },
 };
 
 const filtersReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "CHANGE_FILTER":
+      return {
+        ...state,
+        [action.payload.filterToChange]: {
+          ...state[action.payload.filterToChange],
+          value: action.payload.filterValue,
+        },
+      };
+    case "CLEAR_ALL_FILTERS":
+      return initialState;
     case "CHANGE_PRICE_FILTER":
       return { ...state, byPrice: action.payload };
-    case "CHANGE_COLLECTION_FILTER":
-      return { ...state, byCollection: action.payload };
-    case "CHANGE_PEN_TYPE_FILTER":
-      return { ...state, ByPenType: action.payload };
     default:
       return state;
   }
