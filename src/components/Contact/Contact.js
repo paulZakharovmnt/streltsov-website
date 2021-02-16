651294479772b16c2f53720dd645327021573207
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-02-16 10:28:30 -0500

    Add fade animation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,23 @@
 import React from "react";
 import imageContacts from "../../img/contact/contact.jpg";
 import "./Contact.css";
+import Fade from "react-reveal/Fade";
 
 const Contact = () => {
   return (
     <div className="contact-page">
       <div className="contacts-container">
-        <div className="contacts-info">
-          <h1>Contact</h1>
-          <p> Follow us on Instagram</p>
-          <p> Email us</p>
-        </div>
+        <Fade top cascade distance={"150px"}>
+          <div className="contacts-info">
+            <h1>Contact</h1>
+            <p> Follow us on Instagram</p>
+            <p> Email us</p>
+          </div>
+        </Fade>
         <div className="contact-image">
-          <img src={imageContacts} alt="" />
+          <Fade bottom distance={"150px"}>
+            <img src={imageContacts} alt="" />
+          </Fade>
         </div>
       </div>
     </div>
