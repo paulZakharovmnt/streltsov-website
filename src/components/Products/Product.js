273531df793350475a0044196bb6e2c7602c837f
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-02-16 10:27:47 -0500

    Add fade animation

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
 const Product = ({ listImg, product, productsById }) => {
   const productInfo = productsById[product];
@@ -11,16 +12,18 @@ const Product = ({ listImg, product, productsById }) => {
       className="productByType-list-product-container"
       to={`/streltsov-website/products/${childLink}/${product}`}
     >
-      <div className="productByType-photo">
-        <img src={productInfo.images[0]} alt="" />
-      </div>
-      <div className="productByType-name-price-box">
-        <div className="productByType-name-container">
-          <h4>{productInfo.name}</h4>
-          <p className="test">{productInfo.collection}</p>
+      <Fade delay={300} bottom distance={"100px"}>
+        <div className="productByType-photo">
+          <img src={productInfo.images[0]} alt="" />
         </div>
-        <div className="productByType-price">{productInfo.price} USD</div>
-      </div>
+        <div className="productByType-name-price-box">
+          <div className="productByType-name-container">
+            <h4>{productInfo.name}</h4>
+            <p className="test">{productInfo.collection}</p>
+          </div>
+          <div className="productByType-price">{productInfo.price} USD</div>
+        </div>
+      </Fade>
     </Link>
   );
 };
