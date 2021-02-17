2138f65b9970b9893326e9f7580c96f538b6c42a
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-02-16 21:50:47 -0500

    Add breadcrums back

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
         <div className="App">
           <header>
             <Nav />
-            {/* <BreadCrumbs /> */}
+            <BreadCrumbs />
           </header>
 
           <Switch>
