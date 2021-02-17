063615d11f92eeb0dc148ccf5ac18a4dba25aa07
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-02-16 21:51:53 -0500

    Change breadcrumbs structure

--- src/components/AdditionalComponents/BreadCrumbs.js
+++ src/components/AdditionalComponents/BreadCrumbs.js
@@ -2,36 +2,37 @@ import React from "react";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
 import { Link } from "react-router-dom";
 import "./BreadCrumbs.css";
+import BackBTN from "./BackBTN";
 
 const BreadCrumbs = () => {
   const breadcrumbs = useBreadcrumbs();
+
+  const homepageCrumb = breadcrumbs[1].key;
+  const pathCrumbs = breadcrumbs.slice(2, breadcrumbs.length - 1);
+  const currentCrumb =
+    breadcrumbs[breadcrumbs.length - 1].breadcrumb.props.children;
+
   return (
     <div className="breadCrumbs-container">
       {breadcrumbs.length <= 2 ? null : (
         <div className="breadCrumbs">
-          <Link
-            to={`${breadcrumbs[breadcrumbs.length - 3].key}`}
-            className="back-crumb"
-          >
-            <div className="round">
-              <div id="cta">
-                <span className="arrow primera next "></span>
-                <span className="arrow segunda next "></span>
-              </div>
+          <BackBTN breadcrumbs={breadcrumbs} />
+          <div className="crumbs-path">
+            <Link to={`${homepageCrumb}`} className="home-crumb">
+              Home
+            </Link>
+            <p> &nbsp;/</p>
+            <div className="path">
+              {pathCrumbs.map((crumb) => (
+                <div key={crumb.key} className="crumb-box">
+                  <Link to={`${crumb.key}`} className="crumb">
+                    {crumb.breadcrumb.props.children}
+                  </Link>
+                  <p> /</p>
+                </div>
+              ))}
             </div>
-            <p>Back</p>
-          </Link>
-          {breadcrumbs.slice(1, breadcrumbs.length - 1).map((crumb) => (
-            <div key={crumb.key} className="crumb-box">
-              {" "}
-              <Link to={`${crumb.key}`} className="crumb">
-                {crumb.breadcrumb.props.children}
-              </Link>
-              <p>/</p>
-            </div>
-          ))}
-          <div className="current-crumb">
-            {breadcrumbs[breadcrumbs.length - 1].breadcrumb.props.children}
+            <div className="current-crumb">{currentCrumb}</div>
           </div>
         </div>
       )}
