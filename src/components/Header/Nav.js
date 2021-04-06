6ca5e643ca44abb0ffb27344f5b6dd151e6e9d4d
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-04-06 11:12:29 -0400

    Add redux state to nav

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -2,15 +2,40 @@ import React from "react";
 import "./Nav.css";
 import DesktopNavPanel from "./DesktopNav/DesktopNavPanel";
 import MobileNavPanel from "./MobileNav/MobileNavPanel";
+import getUniqueValues from "../../core/getUniqueValues";
+import { useSelector } from "react-redux";
 
 const Nav = () => {
+  const productCategoriesAllIds = useSelector(
+    (state) => state.settingsReducer.settingsById.productType.allIds
+  );
+  const productCategoriesById = useSelector(
+    (state) => state.settingsReducer.settingsById.productType.byId
+  );
+
+  // console.log(productCategoriesById);
+
+  const uniqueProductCategories = getUniqueValues(
+    productCategoriesAllIds,
+    productCategoriesById,
+    "category"
+  );
+
   return (
     <div className="nav">
       <div className="nav-desktop">
-        <DesktopNavPanel />
+        <DesktopNavPanel
+          uniqueProductCategories={uniqueProductCategories}
+          productsCategories={productCategoriesById}
+          productsCategoriesIds={productCategoriesAllIds}
+        />
       </div>
       <div className="nav-mobile">
-        <MobileNavPanel />
+        <MobileNavPanel
+          uniqueProductCategories={uniqueProductCategories}
+          productsCategories={productCategoriesById}
+          productsCategoriesIds={productCategoriesAllIds}
+        />
       </div>
     </div>
   );
