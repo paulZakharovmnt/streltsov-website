dd10bd56c3738f7e37939c87f657f5eccaf8f4df
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2021-04-06 11:06:39 -0400

    Add new functions in reducer. Add and delete prod

--- src/redux/reducers/productsAllIdsReducer.js
+++ src/redux/reducers/productsAllIdsReducer.js
@@ -1,11 +1,13 @@
-const productsAllIdsReducer = (state = [], action) => {
+import { productsAllIds } from "../../core/TestProducts";
+
+const productsAllIdsReducer = (state = productsAllIds, action) => {
   switch (action.type) {
     case "SET_ALL_PRODUCTS_FROM_FB":
       return Object.keys(action.payload);
-    // case "ADD_CITY_TO_STATE":
-    //   return [action.payload.name, ...state];
-    // case "DELETE_CITY":
-    //   return state.filter((cityName) => cityName !== action.payload);
+    case "ADD_PRODUCT":
+      return [...state, action.payload.name];
+    case "DELETE_PRODUCT":
+      return state.filter((product) => product !== action.payload);
     default:
       return state;
   }
